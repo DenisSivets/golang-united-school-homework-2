4e277489f4acdd0517eeb62da72615e5da5f5a40
DenisSivets <sivkaden@gmail.com>
2022-02-20 22:49:45 +0300

    complete task v3

--- solution.go
+++ solution.go
@@ -6,20 +6,19 @@ type sides int
 
 const (
 	SidesTriangle sides = 3
-	SidesSquare    sides = 4
-	SidesCircle    sides = 0
+	SidesSquare   sides = 4
+	SidesCircle   sides = 0
 )
 
 func CalcSquare(sideLen float64, sidesNum sides) float64 {
-	var square float64
-	if sidesNum == SidesTriangle {
-		square = math.Sqrt(3) * sideLen * sideLen / 4
+	switch sidesNum {
+	case SidesTriangle:
+		return math.Sqrt(3) * sideLen * sideLen / 4
+	case SidesSquare:
+		return sideLen * sideLen
+	case SidesCircle:
+		return math.Pi * sideLen * sideLen
+	default:
+		return 0
 	}
-	if sidesNum == SidesSquare {
-		square = sideLen * sideLen
-	}
-	if sidesNum == SidesCircle {
-		square = math.Pi * sideLen * sideLen
-	}
-	return square
 }
