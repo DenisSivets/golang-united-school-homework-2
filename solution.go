5d9d9fcf7e3dc23c124678f2f718343227554867
DenisSivets <sivkaden@gmail.com>
2022-02-20 16:29:06 +0300

    complete task

--- solution.go
+++ solution.go
@@ -5,21 +5,21 @@ import "math"
 type sides int
 
 const (
-	SidesTriangle sides=3
-	SidesSquar  sides=4
-	SidesCircl  sides=0
+	SidesTriangle sides = 3
+	SidesSquar    sides = 4
+	SidesCircl    sides = 0
 )
 
 func CalcSquare(sideLen float64, sidesNum sides) float64 {
 	var square float64
-	if sidesNum==SidesTriangle{
-		square=math.Sqrt(3) * sideLen * sideLen / 4
+	if sidesNum == SidesTriangle {
+		square = math.Sqrt(3) * sideLen * sideLen / 4
 	}
-	if sidesNum==SidesSquar{
-		square=sideLen*sideLen
+	if sidesNum == SidesSquar {
+		square = sideLen * sideLen
 	}
-	if sidesNum==SidesCircl{
-		square=math.Pi * sideLen * sideLen
+	if sidesNum == SidesCircl {
+		square = math.Pi * sideLen * sideLen
 	}
 	return square
 }
